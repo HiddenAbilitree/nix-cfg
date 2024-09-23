7946c82b473e4fa305446f4c108557adfab5ec31
= <=>
2024-09-23 17:59:31 -0400

    update

--- home/programs/pipewire/default.nix
+++ home/programs/pipewire/default.nix
@@ -1,13 +1,4 @@
 { pkgs, config, ... }:
 {
-  services.pipewire = {
-    enable = true;
-    audio.enable = true;
-    pulse.enable = true;
-    alsa = {
-      enable = true;
-      support32Bit = true;
-    };
-    jack.enable = true;
-  };
+
 }

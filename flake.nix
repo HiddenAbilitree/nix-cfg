90a40b601a7bb6d9f31487c415f8edd0399b6537
= <=>
2024-09-23 15:11:28 -0400

    semi-working

--- flake.nix
+++ flake.nix
@@ -7,14 +7,12 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland.url = "github:hyprwm/Hyprland";
   };
 
   outputs =
     inputs@{
       nixpkgs,
       home-manager,
-      hyprland,
       ...
     }:
     {

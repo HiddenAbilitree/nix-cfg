b5e9e300661fc929fd0948d95b441b4917a10cc0
= <=>
2024-09-23 19:31:42 -0400

    update

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -8,11 +8,10 @@
     autocd = true;
     #initExtra = builtins.readFile ./.zshrc;
     shellAliases = {
-      fuck = "thefuck";
-      cfg = "nvim /home/ezhang/nixos-cfg/";
-      rebuild = "git -C ~/nixos-cfg/ commit -am update & git -C ~/nixos-cfg/ push & sudo nixos-rebuild switch --flake ~/nixos-cfg/#";
+      cfg = "nvim ~/nixos-cfg/";
+      rebuild = "git -C ~/nixos-cfg add ./ & git -C ~/nixos-cfg/ commit -am update & git -C ~/nixos-cfg/ push & sudo nixos-rebuild switch --flake ~/nixos-cfg/#";
       nix-clear = "sudo nix-collect-garbage -d; sudo /run/current-system/bin/switch-to-configuration boot";
-      fetch = "fastfe2tch";
+      fetch = "fastfetch";
     };
     oh-my-zsh = {
       enable = true;
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -131,6 +131,25 @@
     #jack.enable = true;
   };
 
+  services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
+    context.modules = [
+      {
+        name = "libpipewire-module-protocol-pulse";
+        args = {
+          pulse.min.req = "32/48000";
+          pulse.default.req = "32/48000";
+          pulse.max.req = "32/48000";
+          pulse.min.quantum = "32/48000";
+          pulse.max.quantum = "32/48000";
+        };
+      }
+    ];
+    stream.properties = {
+      node.latency = "32/48000";
+      resample.quality = 1;
+    };
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

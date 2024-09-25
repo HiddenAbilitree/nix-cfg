4b50a27745b173b598cabf2ff749ed49a115abff
= <=>
2024-09-25 15:17:22 -0400

    update

--- home/services/gtk/default.nix
+++ home/services/gtk/default.nix
@@ -6,7 +6,7 @@
 {
   gtk = {
     enable = true;
-    gtk.iconTheme.package = lib.mkForce pkgs.papirus-icon-theme;
-    gtk.iconTheme.name = lib.mkForce "Papirus Dark";
+    iconTheme.package = lib.mkForce pkgs.papirus-icon-theme;
+    iconTheme.name = lib.mkForce "Papirus Dark";
   };
 }

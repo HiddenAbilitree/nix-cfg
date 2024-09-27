1525668df63cd31bea814784f1a85435510c3541
= <=>
2024-09-26 21:06:29 -0400

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -127,10 +127,13 @@
   # audio
   security.rtkit.enable = true;
 
+  hardware.pulseaudio.enable = true;
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
-    alsa.support32Bit = true;
+
+    # alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;

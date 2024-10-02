6fe8993ea2501e65cb6c30a1800b6ed5718c2c50
= <=>
2024-10-02 16:54:37 -0400

    update

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -11,7 +11,7 @@
       cfg = "code ~/nixos-cfg/";
       rebuild = "git -C ~/nixos-cfg/ add -A && git -C ~/nixos-cfg/ commit -am update; sudo nixos-rebuild switch --flake ~/nixos-cfg/# && source ~/.zshrc";
       nix-clear = "sudo nix-collect-garbage -d; sudo /run/current-system/bin/switch-to-configuration boot";
-      fetch = "fastfetch";
+      fetch = "fastfetch\nsource /etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh";
       "367" = "cd ~/School/cs367";
       calq = "qalc";
       calc = "qalc";

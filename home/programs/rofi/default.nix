0c2a6c74840f963e5a58b859421efc1a0996dcfb
= <=>
2024-10-02 13:31:45 -0400

    update

--- home/programs/rofi/default.nix
+++ home/programs/rofi/default.nix
@@ -5,6 +5,10 @@
     terminal = "${pkgs.kitty}/bin/kitty";
     package = pkgs.rofi-wayland;
     theme = ./theme.rasi;
+    extraConfig = {
+      kb-mode-next = "Right,Control+Tab";
+      kb-mode-previous = "Left,Shift+Control+Tab";
+    };
   };
   home.file.".config/rofi/theme.rasi".text = builtins.readFile ./theme.rasi;
   home.file.".config/rofi/colors/tokyo-night.rasi".text = builtins.readFile ./colors/tokyo-night.rasi;

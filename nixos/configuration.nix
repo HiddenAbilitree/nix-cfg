e110e176e04c5a7fa87cbf4a18248fb6abbfb83b
= <=>
2024-10-04 12:03:56 -0400

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -128,6 +128,12 @@
 
   services.gnome.core-utilities.enable = false;
 
+  documentation = {
+    enable = true;
+    man.enable = true;
+    dev.enable = true;
+  };
+
   # audio
   security.rtkit.enable = true;
 

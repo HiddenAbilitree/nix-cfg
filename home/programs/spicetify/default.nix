a455f0814feec99921fa88a26ae34efebf48e240
= <=>
2024-10-10 12:33:49 -0400

    update

--- home/programs/spicetify/default.nix
+++ home/programs/spicetify/default.nix
@@ -19,6 +19,6 @@
         shuffle # shuffle+ (special characters are sanitized out of extension names)
       ];
       theme = spicePkgs.themes.catppuccin;
-      colorScheme = "mocha";
+      colorScheme = "macchiato";
     };
 }

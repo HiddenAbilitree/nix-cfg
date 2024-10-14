5beb3dfa4106777557711557b3229befc815b875
Eric Zhang <eric@ericzhang.dev>
2024-10-14 14:12:04 -0400

    update

--- fw13/home/programs/hyprland/default.nix
+++ fw13/home/programs/hyprland/default.nix
@@ -8,22 +8,22 @@
     hyprland = {
       enable = true;
       extraConfig = builtins.readFile ./hyprland.conf;
-      bind =
-        [
-
-        ]
-        ++ (builtins.concatLists (
-          builtins.genList (
-            i:
-            let
-              ws = i + 1;
-            in
-            [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-            ]
-          ) 9
-        ));
+      settings = {
+        bind = (
+          builtins.concatLists (
+            builtins.genList (
+              i:
+              let
+                ws = i + 1;
+              in
+              [
+                "$mod, code:1${toString i}, workspace, ${toString ws}"
+                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+              ]
+            ) 9
+          )
+        );
+      };
     };
   };
 }

2b3c3167397ddecd6a753dcc92e7de0267e7f89c
Eric Zhang <eric@ericzhang.dev>
2024-10-14 20:13:33 -0400

    update

--- home/programs/hypr/default.nix
+++ home/programs/hypr/default.nix
@@ -6,10 +6,6 @@
 {
   wayland.windowManager = {
     hyprland = {
-      "$menu" = "rofi -show drun";
-      "$mod" = "SUPER";
-      "$browser" = "firefox";
-      "$terminal" = "kitty";
       enable = true;
       extraConfig = builtins.readFile ./hyprland.conf;
     };

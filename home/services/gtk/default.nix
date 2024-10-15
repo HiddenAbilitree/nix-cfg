f9fe3ba3253d517eec6e74cf4e49b4d52d9613a6
Eric Zhang <eric@ericzhang.dev>
2024-10-15 15:43:39 -0400

    update

--- home/services/gtk/default.nix
+++ home/services/gtk/default.nix
@@ -6,10 +6,7 @@
 {
   gtk = {
     enable = true;
-    theme = {
-      name = "Tokyonight-Dark";
-      package = pkgs.tokyonight-gtk-theme;
-    };
+
     # iconTheme = {
     #   name = "Tokyonight-Dark";
     #   package = pkgs.tokyonight-icon-theme;

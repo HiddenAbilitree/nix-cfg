a32ab09cc2d81a2698f2b2a8b1d9e047634bf794
Eric Zhang <eric@ericzhang.dev>
2024-10-14 21:54:34 -0400

    update

--- fw13/home/programs/waybar/default.nix
+++ fw13/home/programs/waybar/default.nix
@@ -1,6 +1,6 @@
 { pkgs, config, ... }:
 {
   programs.waybar = {
-    extraConfig = builtins.readFile ./waybar.conf;
+
   };
 }
--- home/programs/waybar/default.nix
+++ home/programs/waybar/default.nix
@@ -132,7 +132,36 @@
             persistent = "";
             empty = "";
           };
-
+          "persistent-workspaces" =
+            if config.networking.hostName == "loser" then
+              {
+                "*" = 9;
+              }
+            else
+              {
+                "DP-1" = [
+                  11
+                  12
+                  13
+                  14
+                  15
+                  16
+                  17
+                  18
+                  19
+                ];
+                "DP-2" = [
+                  1
+                  2
+                  3
+                  4
+                  5
+                  6
+                  7
+                  8
+                  9
+                ];
+              };
         };
         pulseaudio = {
           scroll-step = 1;

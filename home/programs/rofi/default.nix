f8cb83aaadb2f48e605782f6d78885750a5ae624
Eric Zhang <eric@ericzhang.dev>
2024-10-17 12:39:13 -0400

    update

--- home/programs/rofi/default.nix
+++ home/programs/rofi/default.nix
@@ -3,7 +3,7 @@
   programs.rofi = {
     enable = true;
     terminal = "${pkgs.kitty}/bin/kitty";
-    font = "Dejavu Serif";
+    font = "DejaVu Serif";
     package = pkgs.rofi-wayland;
     theme = ./theme.rasi;
     extraConfig = {
